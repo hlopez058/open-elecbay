dd7b7d3798af40c2e2eb1b5eb5eea22c1494c788
Hector Lopez <>
2021-07-22 15:34:18 -0400

    adding another app

---
+++ core/app.py
@@ -0,0 +1,15 @@
+
+
+# Example market requests
+# Should be managed as a queue by a brokerage thread
+##
+MARKET_REQUESTS = {
+    "8c36e86c-13b9-4102-a44f-646015dfd981": {
+        'type': 'INFO',
+        'message': u'Test request',
+        'timestamp': (datetime.today() - timedelta(1)).timestamp()
+    }
+}
+
+
+def
